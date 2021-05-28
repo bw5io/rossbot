b0fbca84056cb75c1c763f938876ba72b360348f
Bill Wu <hi@bw5.io>
2021-05-28 22:08:11 +0100

    database

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
-const { Client, MessageEmbed } = require('discord.js');
+const { Client, MessageEmbed, VoiceConnection } = require('discord.js');
 const axios = require('axios');
 const client = new Client();
-
+const mysql = require('mysql');
 const prefix = '!problem';
 const problemUrlBase = 'https://leetcode.com/problems/';
 const ltApiUrl = 'https://leetcode.com/api/problems/all/';
@@ -10,12 +10,28 @@ const freeProblems = [];
 const paidProblems = [];
 let totalProblems;
 
+// Connect to database
+
+var pool = mysql.createPool({
+	host: process.env.MYSQL_SRV_ADDRESS,
+	user: process.env.MYSQL_SRV_USR,
+	password: process.env.MYSQL_SRV_PW,
+	database: 'dcbot'
+  });
+  
+pool.getConnection(function(err, conn) {
+	if (err) throw err;
+	console.log("Connected!");
+	conn.release();
+});
+
 /**
  * Returns a random number based on provided max constraint.
  * @param {int} max
  */
 function getRandomInt(max) {
-	return Math.floor(Math.random() * Math.floor(max));
+	let randomizedNumber = Math.floor(Math.random() * Math.floor(max));
+	return randomizedNumber;
 }
 
 /**
@@ -77,9 +93,12 @@ function problemType(data, msg, diff = '') {
 	}
 	const dataLen = data.length;
 	const randProblem = getRandomInt(dataLen);
+	let server = msg.guild.id;   // get server id
 	const aProblem = data[randProblem];
+	console.log(server);
+	console.log(aProblem.id);
+	checkExist(server, aProblem.id);
 	const problemUrl = problemUrlBase + aProblem.titleSlug + '/';
-
 	const embed = new MessageEmbed()
 		.setTitle(aProblem.title)
 		.setColor('#f89f1b')
@@ -93,9 +112,42 @@ function problemType(data, msg, diff = '') {
 	msg.channel.send(embed);
 }
 
+async function checkExist(ServerID, ChallengeID){
+	let output = pool.getConnection(function(err,conn){
+		if (err) throw err;
+		conn.query("SELECT * FROM attempted_challenges WHERE server_id = ? AND challenge_id = ?", [ServerID, ChallengeID], function(err, result){
+			let output;
+			if (err) throw err;
+			console.log(result);
+			if (result.length===0)
+			{
+				pool.getConnection(function(err, conn) {
+					if (err) throw err;
+					console.log("Connected!");
+					var sql = "INSERT INTO attempted_challenges (server_id, challenge_id) VALUES ?";
+					var values = [
+					  [ServerID, ChallengeID],
+					];
+					conn.query(sql, [values], function (err, result) {
+					  if (err) throw err;
+					  console.log("Number of records inserted: " + result.affectedRows);
+					});
+					conn.release();
+				});
+			}
+			else {
+				output=true;
+				console.log(output);
+			}
+			conn.release();
+		});
+	});
+	console.log(output);
+	return output;
+}
+
 client.on('message', (msg) => {
 	if (!msg.content.startsWith(prefix) || msg.author.bot) return;
-
 	const args = msg.content.slice(prefix.length).trim().split(' ');
 	const command = args.shift().toLowerCase();
 	let diff;
@@ -125,6 +177,10 @@ client.on('message', (msg) => {
 			'\n\n\t!problem paid - gives you a random paid/locked problem of any difficulty.' +
 			'\n\nAdding difficulty modifiers:\n\n\t!problem <free | paid> <easy | medium | hard> - lets you pick a random free or paid problem of the chosen difficulty.```',
 		);
+	}
+	else if (command === 'debug'){
+		msg.channel.send(checkExist(0,0));
+
 	}
 	else {
 		problemType(allProblems, msg, diff);
